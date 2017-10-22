6a1ac3544b68831d924442887656e61ed20a0fbe
M Safdar Ali Khan <msafdaralikhan65@gmail.com>
2017-10-22 20:59:08 +0500

    Added relation b/w user and books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022151453) do
+ActiveRecord::Schema.define(version: 20171022153026) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171022151453) do
     t.string   "author"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
