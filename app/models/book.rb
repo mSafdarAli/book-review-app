33faa55dc2851bcb24ac2182daf990abf87e0cbb
M Safdar Ali Khan <msafdaralikhan65@gmail.com>
2017-10-25 23:06:10 +0500

    Added rating and average rating system

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
+	has_many :reviews
 	
   	has_attached_file :image, :styles => { :book_index => "250x350>", :book_show => "352x475>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
